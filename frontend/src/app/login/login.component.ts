3610de2aceea05e5397a08217c4b8eb77e64bf0b
Daniel Piñeiro Placer <38256338+danipineiro@users.noreply.github.com>
2024-04-11 00:08:19 +0200

    basic login

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatCard, MatCardContent} from "@angular/material/card"
+import {MatButtonModule} from '@angular/material/button';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatCard, MatCardContent, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
