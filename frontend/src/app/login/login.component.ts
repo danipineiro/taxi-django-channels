ae3a2084e74e29d5a447b6531bfb319b6817ded1
Daniel Piñeiro Placer <38256338+danipineiro@users.noreply.github.com>
2024-05-06 22:03:53 +0200

    Login
    New design

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -4,12 +4,13 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatCard, MatCardContent} from "@angular/material/card"
 import {MatButtonModule} from '@angular/material/button';
+import {MatDivider} from "@angular/material/divider";
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatCard, MatCardContent, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatCard, MatCardContent, MatButtonModule, MatDivider],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
