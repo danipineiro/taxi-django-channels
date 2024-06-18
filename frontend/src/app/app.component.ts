35c45a34088a9e2b44ada16695850659a5310fd0
Daniel Piñeiro Placer <38256338+danipineiro@users.noreply.github.com>
2024-06-18 19:16:16 +0200

    main toolbar when user is logged in

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Router, RouterOutlet} from '@angular/router';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIcon} from "@angular/material/icon";
+import {AuthService} from "./services/auth.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatSlideToggleModule],
+  imports: [CommonModule, RouterOutlet, MatSlideToggleModule, MatToolbar, MatIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-docker';
+
+  isLoggedIn = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) {
+  }
+
+  ngOnInit() {
+    this.isLoggedIn = this.authService.isLogged();
+  }
+
+  logout() {
+    this.authService.logout().subscribe({
+      next: (response: any) => {
+        console.log(response);
+        this.isLoggedIn = false;
+        this.router.navigate(['/login']);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
 }
