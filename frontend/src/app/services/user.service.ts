b6bf2996d1d9e39ef3aba05cf8e13a4041afe815
Daniel Piñeiro Placer <38256338+danipineiro@users.noreply.github.com>
2024-08-21 20:25:59 +0200

    environments for frontend

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,17 +1,19 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:8000/api/v1';
+  private apiUrl = `${environment.host}api/v1/`;
 
   constructor(private http: HttpClient) {
   }
 
   getCurrentUser(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/profile/`,);
+    return this.http.get(`${this.apiUrl}profile/`,);
   }
 }
