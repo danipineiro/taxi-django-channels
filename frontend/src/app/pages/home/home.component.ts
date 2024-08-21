86f265bfc1512ab2cec13878fda336146e832365
Daniel Piñeiro Placer <38256338+danipineiro@users.noreply.github.com>
2024-08-21 21:08:30 +0200

    get trips on home

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatAnchor, MatButton} from "@angular/material/button";
 import {TripService} from "../../services/trip.service";
+import {CurrentUserDTO} from "../../models/current-user-dto";
+import {DRIVER, PASSENGER} from "../../shared/constants/user-types";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-home',
@@ -12,16 +15,41 @@ import {TripService} from "../../services/trip.service";
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public trips: any[] = [];
 
   constructor(
     private readonly tripService: TripService,
+    private userService: UserService
   ) {
+  }
+
+    ngOnInit() {
+    this.userService.getCurrentUser().subscribe((user: CurrentUserDTO) => {
+      if (user.type === DRIVER) {
+        this.getDriverTrips();
+      } else if (user.type === PASSENGER) {
+        this.getPassengerTrips();
+      }
+    });
   }
 
   createTrip() {
     this.tripService.createTrip().subscribe((trip) => {
       console.log(trip);
+    });
+  }
+
+    getPassengerTrips() {
+    this.tripService.getPassengerTrips().subscribe((trips) => {
+      this.trips = trips;
+      console.log(trips);
+    });
+  }
+
+  getDriverTrips() {
+    this.tripService.getDriverTrips().subscribe((trips) => {
+      this.trips = trips;
     });
   }
 }
