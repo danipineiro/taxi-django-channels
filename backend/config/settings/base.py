225b741e268c549a88c1cefc79699b3056f51d0a
Daniel Piñeiro <dpinheiro88@gmail.com>
2024-10-28 08:43:12 +0100

    postgres

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
         "NAME": os.environ.get("SQL_DATABASE", "db.sqlite3"),
         "USER": os.environ.get("SQL_USER", "user"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
